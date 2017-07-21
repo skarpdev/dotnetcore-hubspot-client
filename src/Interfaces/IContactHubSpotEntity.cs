0559fb80f7aede1b8d82c8235bd7138ef3341bf7
Nicklas Laine Overgaard <nicklas@skarp.dk>
2017-07-21 11:24:00 +0200

    Add basic interface and interface for contacts

---
+++ src/Interfaces/IContactHubSpotEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace Skarp.HubSpotClient.Interfaces
+{
+    public interface IContactHubSpotEntity : IHubSpotEntity
+    {
+        string Email { get; set; }
+        string FirstName { get; set; }
+        string Lastname { get; set; }
+        string Website { get; set; }
+        string Company { get; set; }
+        string Phone { get; set; }
+        string Address { get; set; }
+        string City { get; set; }
+        string State { get; set; }
+        string ZipCode { get; set; }
+    }
+}
---
+++ src/Interfaces/IHubSpotEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Skarp.HubSpotClient.Interfaces
+{
+    public interface IHubSpotEntity
+    {
+
+        /// <summary>
+        /// When implemented in a downstream class it should return the HubSpot route that the entity belongs to,
+        /// e.g /companies/v2/companies/
+        /// </summary>
+        /// <returns></returns>
+        string RoutePath { get;  }
+    }
+}
