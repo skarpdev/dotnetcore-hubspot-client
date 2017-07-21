64e011faa3bc25d0b1cbfe381f8ba6a20ef03452
Nicklas Laine Overgaard <nicklas@skarp.dk>
2017-07-21 23:42:20 +0200

    Implement parsing FROM a hubspot response

--- src/Requests/RequestDataConverter.cs
+++ src/Requests/RequestDataConverter.cs
@@ -1,5 +1,10 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 using Skarp.HubSpotClient.Interfaces;
 
 namespace Skarp.HubSpotClient.Requests
@@ -46,13 +51,56 @@ namespace Skarp.HubSpotClient.Requests
                     item.Property = null;
                     item.Name = prop.Name;
                 }
-                if(item.Value == null) continue;
+                if (item.Value == null) continue;
 
                 mapped.Properties.Add(item);
             }
 
             _logger.LogDebug("Mapping complete, returning data");
             return mapped;
+        }
+
+        /// <summary>
+        /// Convert from the dynamicly typed <see cref="ExpandoObject"/> into a strongly typed <see cref="IHubSpotEntity"/>
+        /// </summary>
+        /// <param name="dynamicObject">The <see cref="ExpandoObject"/> representation of the returned json</param>
+        /// <returns></returns>
+        public T FromHubSpotResponse<T>(ExpandoObject dynamicObject) where T : IHubSpotEntity, new()
+        {
+            var expandoDict = (IDictionary<string, object>) dynamicObject;
+
+            var data = new T();
+            var dataProps = data.GetType().GetProperties();
+
+            if (expandoDict.TryGetValue("vid", out var vidData))
+            {
+                var vidProp = dataProps.SingleOrDefault(q => q.Name.ToLowerInvariant() == "vid");
+                vidProp?.SetValue(data, vidData);
+            }
+
+            if (!expandoDict.TryGetValue("properties", out var dynamicProperties))
+            {
+                throw new ArgumentException("The given JSON document does not contain a properties object");
+            }
+            
+            foreach (var dynamicProp in dynamicProperties as ExpandoObject)
+            {
+                // prop.Key contains the name of the property we wish to map into the DTO
+                // prop.Value contains the data returned by HubSpot, which is also an object 
+                // in there we need to go get the "value" prop to get the actual value
+                _logger.LogDebug("Looking at dynamic prop: {0}", dynamicProp.Key);
+                
+                if (!((IDictionary<string, Object>) dynamicProp.Value).TryGetValue("value", out object dynamicValue))
+                {
+                    continue;
+                }
+                
+                var targetProp =
+                    dataProps.SingleOrDefault(q => q.Name.ToLowerInvariant() == dynamicProp.Key.ToLowerInvariant());
+                _logger.LogDebug("Have target prop? '{0}' with name: '{1}' and actual value: '{2}'", targetProp != null, targetProp?.Name ?? "N/A", dynamicValue);
+                targetProp?.SetValue(data, dynamicValue);
+            }
+            return data;
         }
     }
 }
--- test/unit/Requests/RequestDataConverterTest.cs
+++ test/unit/Requests/RequestDataConverterTest.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System.Dynamic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Skarp.HubSpotClient.Dto;
 using Skarp.HubSpotClient.Requests;
 using Xunit;
@@ -29,7 +32,7 @@ namespace Skarp.HubSpotClient.UnitTest
                 Website = "http://hubspot.com",
                 ZipCode = "02139"
             };
-            
+
             _customContactDto = new CustomContactHubSpotEntity
             {
                 Address = "25 First Street",
@@ -79,7 +82,7 @@ namespace Skarp.HubSpotClient.UnitTest
             Assert.NotEmpty(converted.Properties);
 
             Assert.True(converted.Properties.Count == 11, $"converted.Properties.Count == 11, count is {converted.Properties.Count}");
-            
+
             // check that the custom prop was properly mapped
             var prop = converted.Properties.Single(q => q.Property == "MyCustomProp");
             Assert.Equal(prop.Value, "Has a value!");
@@ -89,11 +92,11 @@ namespace Skarp.HubSpotClient.UnitTest
         public void RequestDataConvert_converts_company_to_internal_repsentation()
         {
             var converted = _converter.ToHubspotDataEntity(_companyDto);
-            
+
             Assert.NotNull(converted);
             var allProps = converted.Properties;
             Assert.NotEmpty(allProps);
-            
+
             Assert.True(allProps.Count == 2, "converted.Properties.Count == 2");
 
             foreach (var prop in allProps)
@@ -101,6 +104,22 @@ namespace Skarp.HubSpotClient.UnitTest
                 Assert.NotNull(prop.Name);
                 Assert.NotNull(prop.Value);
             }
+        }
+
+        [Fact]
+        public void RequestDataConvert_converts_response_to_entity()
+        {
+            var json = "{'identity-profiles':[{'identities': [{'timestamp': 1331075050646,'type':'EMAIL','value':'fumanchu@hubspot.com'},{'timestamp': 1331075050681,'type': 'LEAD_GUID','value': '22a26060-c9d7-44b0-9f07-aa40488cfa3a'      }    ],    'vid': 61574  }],'properties': {  'website': {    'value': 'http://hubspot.com',    'versions': [      {        'timestamp': 1331075050646,        'selected':false,        'source-label':\"None\",        'value': 'http: //hubspot.com',        'source-type': 'API',        'source-id':\"None\"      }    ]  },  'city': {    'value': 'Cambridge',    'versions': [      {        'timestamp': 1331075050646,        'selected':false,        'source-label':\"None\",        'value': 'Cambridge',        'source-type': 'API',        'source-id':\"None\"      }    ]  },  'firstname': {    'value': 'Adrian',    'versions': [      {        'timestamp': 1331075050646,        'selected':false,        'source-label':\"None\",        'value': 'Adrian',        'source-type': 'API',        'source-id':\"None\"      }    ]  },  'zipcode': {    'value': '02139',    'versions': [      {        'timestamp': 1331075050646,        'selected':false,        'source-label':\"None\",        'value': '02139',        'source-type': 'API',        'source-id':\"None\"      }    ]  },  'lastname': {    'value': 'Mott',    'versions': [      {        'timestamp': 1331075050646,        'selected':false,        'source-label':\"None\",        'value': 'Mott',        'source-type': 'API',        'source-id':\"None\"      }    ]  },  'company': {    'value': 'HubSpot',    'versions': [      {        'timestamp': 1331075050646,        'selected':false,        'source-label':\"None\",        'value': 'HubSpot',        'source-type': 'API',        'source-id':\"None\"      }    ]  },  'phone': {    'value': '555-122-2323',    'versions': [      {        'timestamp': 1331075050646,        'selected':false,        'source-label':\"None\",        'value': '555-122-2323',        'source-type': 'API',        'source-id':\"None\"      }    ]  },  'state': {    'value': 'MA',    'versions': [      {        'timestamp': 1331075050646,        'selected':false,        'source-label':\"None\",        'value': 'MA',        'source-type': 'API',        'source-id':\"None\"      }    ]  },  'address': {    'value': '25FirstStreet',    'versions': [      {        'timestamp': 1331075050646,        'selected':false,        'source-label':\"None\",        'value': '25FirstStreet',        'source-type': 'API',        'source-id':\"None\"      }    ]  },  'email': {    'value': 'fumanchu@hubspot.com',    'versions': [      {        'timestamp': 1331075050646,        'selected':false,        'source-label':\"None\",        'value': 'fumanchu@hubspot.com',        'source-type': 'API',        'source-id':\"None\"}]}},'form-submissions': [],'vid': 61574}";
+            var jobj = JsonConvert.DeserializeObject<ExpandoObject>(json);
+
+            var contact = _converter.FromHubSpotResponse<ContactHubSpotEntity>(jobj);
+            Assert.NotNull(contact);
+
+            Assert.Equal(contact.Vid, 61574);
+            Assert.Equal(contact.Website, "http://hubspot.com");
+            Assert.Equal(contact.Email, "fumanchu@hubspot.com");
+            Assert.Equal(contact.FirstName, "Adrian");
+            Assert.Equal(contact.ZipCode, "02139");
         }
 
         private class CustomContactHubSpotEntity : ContactHubSpotEntity
