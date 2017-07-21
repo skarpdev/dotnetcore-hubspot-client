4d901ae42a9e2f2cd593e625249d24f31cbc4799
Nicklas Laine Overgaard <nicklas@skarp.dk>
2017-07-21 23:46:53 +0200

    Add enumeration for possible HubSpot actions
    Like Get, list, update and so on

---
+++ src/HubSpotAction.cs
@@ -0,0 +1,18 @@
+﻿namespace Skarp.HubSpotClient
+{
+    /// <summary>
+    /// Enumerates the possible actions against the hubspot api
+    /// </summary>
+    public enum HubSpotAction
+    {
+        Create,
+
+        Get,
+
+        List,
+
+        Update,
+
+        Delete
+    }
+}
