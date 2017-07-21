827efc65af7a28ae6abc42b9929768e8d41ed4d4
Nicklas Laine Overgaard <nicklas@skarp.dk>
2017-07-21 12:17:52 +0200

    Add dto for that represents hubspot api endpoint data
    Their weird { properties: [] } data representation

---
+++ src/Requests/HubspotDataEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Skarp.HubSpotClient.Requests
+{
+    /// <summary>
+    /// Class that represents the input data model for hubspot API endpoints
+    /// </summary>
+    internal class HubspotDataEntity
+    {
+        /// <summary>
+        /// Gets or sets the properties.
+        /// </summary>
+        /// <value>
+        /// The properties.
+        /// </value>
+        public List<HubspotDataEntityProp> Properties { get; protected set; } = new List<HubspotDataEntityProp>();
+    }
+}
---
+++ src/Requests/HubspotDataEntityProp.cs
@@ -0,0 +1,12 @@
+﻿namespace Skarp.HubSpotClient.Requests
+{
+    /// <summary>
+    /// Represents a property value instance that can be sent to HubSpot
+    /// </summary>
+    internal class HubspotDataEntityProp
+    {
+        public string Property { get; set; }
+        
+        public string Value { get; set; }
+    }
+}
