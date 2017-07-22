5ea79bf1f2ee0747031f4b0bb306fdc69ed5fcbe
Nicklas Laine Overgaard <nicklas@skarp.dk>
2017-07-22 11:21:03 +0200

    Create FunctionalTestBase and add deps to functional tests

---
+++ test/functional/FunctionalTestBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using RapidCore.Xunit.Logging;
+using Xunit.Abstractions;
+
+namespace Skarp.HubSpotClient.FunctionalTests
+{
+    public abstract class FunctionalTestBase<T>
+    {
+        protected readonly ITestOutputHelper Output;
+        protected readonly LoggerFactory LoggerFactory;
+        protected readonly ILogger<T> Logger;
+
+        protected FunctionalTestBase(ITestOutputHelper output)
+        {
+            Output = output;
+            LoggerFactory = new LoggerFactory();
+            LoggerFactory.AddXunitOutput(output);
+
+            Logger = LoggerFactory.CreateLogger<T>();
+        }
+    }
+}
