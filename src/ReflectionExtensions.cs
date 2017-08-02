3a0e1d4c8b48545799260e1a0acba0039ba04ed6
Nicklas Laine Overgaard <nicklas@skarp.dk>
2017-08-02 17:04:36 +0200

    Add Reflection Extensions
    For:
    
    - Resolving a method from implemented interfaces on a given type
    - Get the "serialization" name of a property by using it's name or DataMember attribute

---
+++ src/ReflectionExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using RapidCore.Reflection;
+
+namespace Skarp.HubSpotClient
+{
+    internal static class ReflectionExtensions
+    {
+        /// <summary>
+        /// Returns the name of a given property either by name of <see cref="DataMemberAttribute"/>
+        /// </summary>
+        /// <remarks>
+        /// If the <see cref="DataMemberAttribute"/> is defined it will try to use the prop name defined there. If no name is explictily defined in the attribute the 
+        /// name of the actual property will be returned.
+        /// </remarks>
+        /// <param name="prop"></param>
+        /// <returns></returns>
+        internal static string GetPropSerializedName(this PropertyInfo prop)
+        {
+            if (prop == null) return null;
+
+            var propName = prop.Name;
+
+            var dataMemberAttr = prop.GetCustomAttribute<DataMemberAttribute>();
+            if (dataMemberAttr == null) return propName;
+            if (string.IsNullOrWhiteSpace(dataMemberAttr.Name)) return propName;
+
+            return dataMemberAttr.Name;
+        }
+
+        /// <summary>
+        /// Finds the method recursively by searching in the given type and all implemented interfaces.
+        /// </summary>
+        /// <param name="prop">The property.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="typeArgs">The type arguments.</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        internal static MethodInfo FindMethodRecursively(this Type prop, string name, params Type[] typeArgs)
+        {
+            if (prop == null) return null;
+
+            var method = prop.GetMethod(name, typeArgs);
+            if (method != null) return method;
+
+
+            foreach (var iface in prop.GetInterfaces())
+            {
+                method = iface.FindMethodRecursively(name, typeArgs);
+                if (method != null) return method;
+            }
+            // base types
+            var baseType = prop.GetTypeInfo().BaseType;
+            if (baseType != null)
+            {
+                method = baseType.FindMethodRecursively(name, typeArgs);
+                if (method != null) return method;
+            }
+
+            // TODO better bailout exception?
+            throw new ArgumentException($"Unable to locate method with name: {name}");
+        }
+    }
+}
---
+++ test/unit/ReflectionExtensionTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using Skarp.HubSpotClient.Dto;
+using Skarp.HubSpotClient.Requests;
+using Xunit;
+
+namespace Skarp.HubSpotClient.UnitTest
+{
+    public class ReflectionExtensionTest
+    {
+
+
+        [Fact]
+        public void ReflectionExtension_resolves_prop_names_correctly()
+        {
+            var dto = new ClassWithDataMembers();
+            var dtoProps = dto.GetType().GetProperties();
+
+            var propNoCustomName = dtoProps.Single(p => p.Name == "MemberNoCustomName");
+            Assert.Equal("MemberNoCustomName", propNoCustomName.GetPropSerializedName());
+            var propWithCustomName = dtoProps.Single(p => p.Name == "MemberWithCustomName");
+            Assert.Equal("CallMeThis", propWithCustomName.GetPropSerializedName());
+
+        }
+
+        [Fact]
+        public void ReflectionExtension_can_find_methods_from_base_types_and_interfaces()
+        {
+            var dto = new ContactListHubSpotEntity<ContactHubSpotEntity>();
+
+            // find the Add(T) method on the List entity!
+            var listProp = dto.GetType().GetProperties().Single(p => p.Name == "Contacts");
+            var method = listProp.PropertyType.FindMethodRecursively("Add", new [] {typeof(ContactHubSpotEntity) });
+
+            Assert.NotNull(method);
+        }
+
+        [DataContract]
+        private class ClassWithDataMembers
+        {
+
+            [DataMember()]
+            public string MemberNoCustomName { get; set; }
+
+            [DataMember(Name = "CallMeThis")]
+            public string MemberWithCustomName { get; set; }
+        }
+    }
+}
