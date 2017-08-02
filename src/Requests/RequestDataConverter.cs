0b9f448f06606a17b681cb81ad407d493e08da30
Nicklas Laine Overgaard <nicklas@skarp.dk>
2017-08-02 17:10:35 +0200

    Add support for deserializing LIST responses
    The HubSpot list responses are different kind of beasts so they require
    different handling. Purely generic stuff has been implemented to deal with
    parsing the root level "companies" or "contacts" or what ever HubSpot
    throws into the root level JSON object.
    
    It's not depending on the names of the props in the DTO, but rather IList types
    and generic arguments that are decendandts of IHubSpotEntity so it should handle
    anything.

--- src/Requests/RequestDataConverter.cs
+++ src/Requests/RequestDataConverter.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.Serialization;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
+using RapidCore.Reflection;
 using Skarp.HubSpotClient.Interfaces;
 
 namespace Skarp.HubSpotClient.Requests
@@ -67,40 +70,139 @@ namespace Skarp.HubSpotClient.Requests
         /// <returns></returns>
         public T FromHubSpotResponse<T>(ExpandoObject dynamicObject) where T : IHubSpotEntity, new()
         {
-            var expandoDict = (IDictionary<string, object>) dynamicObject;
+            var data = (T)ConvertSingleEntity(dynamicObject, new T());
+            return data;
+        }
 
+        public T FromHubSpotListResponse<T>(ExpandoObject dynamicObject) where T : IHubSpotEntity, new()
+        {
+            // get a handle to the underlying dictionary values of the ExpandoObject
+            var expandoDict = (IDictionary<string, object>)dynamicObject;
+
+            // For LIST contacts the "contacts" property should be populated, for LIST companies the "companies" property should be populated, and so on
+            // in our T item, search for a property that is an IList<IHubSpotEntity> and use that as our prop name selector into the DynamoObject.....
+            // So on the IContactListHubSpotEntity we have a IList<IHubSpotEntity> Contacts - find that prop, lowercase to contacts and that prop should
+            // be in the DynamoObject from HubSpot! Tricky stuff
+            var targetType = typeof(IHubSpotEntity);
             var data = new T();
             var dataProps = data.GetType().GetProperties();
+            var dataTargetProp = dataProps.SingleOrDefault(p => targetType.IsAssignableFrom(p.PropertyType.GenericTypeArguments.FirstOrDefault()));
 
-            if (expandoDict.TryGetValue("vid", out var vidData))
+            if (dataTargetProp == null)
             {
-                var vidProp = dataProps.SingleOrDefault(q => q.Name.ToLowerInvariant() == "vid");
-                vidProp?.SetValue(data, vidData);
+                throw new ArgumentException("Unable to locate a property on the data class that implements IList<T> where T is a IHubSpotEntity");
             }
 
+            var propSerializedName = dataTargetProp.GetPropSerializedName();
+            if (!expandoDict.ContainsKey(propSerializedName))
+            {
+                throw new ArgumentException($"The json data does not contain a property of name {propSerializedName} which is required to decode the json data");
+            }
+
+            // Find the generic type for the List in question
+            var genericEntityType = dataTargetProp.PropertyType.GenericTypeArguments.First();
+            // get a handle to Add on the list (actually from ICollection)
+            var listAddMethod = dataTargetProp.PropertyType.FindMethodRecursively("Add", genericEntityType);
+            // Condensed version of : https://stackoverflow.com/a/4194063/1662254
+            var listInstance =
+                Activator.CreateInstance(typeof(List<>).MakeGenericType(genericEntityType));
+            if (listAddMethod == null)
+            {
+                throw new ArgumentException("Unable to locate Add method on the list of items to deserialize into - is it an IList?");
+            }
+
+            // Convert all the entities
+            var jsonEntities = expandoDict[propSerializedName];
+            foreach (var entry in jsonEntities as List<object>)
+            {
+                // convert single entity
+                var expandoEntry = entry as ExpandoObject;
+                var dto = ConvertSingleEntity(expandoEntry, Activator.CreateInstance(genericEntityType));
+                // add entity to list
+                listAddMethod.Invoke(listInstance, new[] { dto });
+            }
+            // assign our reflected list instance onto the data object
+            dataTargetProp.SetValue(data, listInstance);
+
+            var allPropNamesInSerializedFormat = GetAllPropsWithSerializedNameAsKey(dataProps);
+            // Now try to map any remaining props from the dynamo object into the response dto we shall return
+            foreach (var kvp in expandoDict)
+            {
+                // skip the property with all the items for the response as we have already mapped that
+                if (kvp.Key == propSerializedName) continue;
+
+                // The Key of the current item should be mapped, so we have to find a property in the target dto that "Serializes" into this value...
+                if (!allPropNamesInSerializedFormat.TryGetValue(kvp.Key, out PropertyInfo theProp))
+                {
+                    continue;
+                }
+                // we have a property which name serializes to the kvp.Key, let's set the data
+                theProp.SetValue(data, kvp.Value);
+            }
+
+            return data;
+        }
+
+        private IDictionary<string, PropertyInfo> GetAllPropsWithSerializedNameAsKey(PropertyInfo[] dataProps)
+        {
+            var dict = new Dictionary<string, PropertyInfo>();
+            foreach (var prop in dataProps)
+            {
+                var propName = prop.GetPropSerializedName();
+                dict.Add(propName, prop);
+            }
+            return dict;
+        }
+
+        /// <summary>
+        /// Converts a single "dynamic" representation of an entity into a typed entity
+        /// </summary>
+        /// <remarks>
+        /// The dynamic object being passed in should have a prop called "properties" which contains all the object properties to map, as well
+        /// as vid and other root level objects stored in the HubSpot JSON response
+        /// </remarks>
+        /// <param name="dynamicObject">An <see cref="ExpandoObject"/> instance that contains a single HubSpot entity to deserialize</param>
+        /// <param name="dto">An instantiated DTO that shall recieve data</param>
+        /// <returns>The populated DTO</returns>
+        internal object ConvertSingleEntity(ExpandoObject dynamicObject, object dto)
+        {
+            var expandoDict = (IDictionary<string, object>)dynamicObject;
+            var dtoProps = dto.GetType().GetProperties();
+
+            // The Properties object in the json / response data contains all the props we wish to map - if that does not exist
+            // we cannot proceeed
             if (!expandoDict.TryGetValue("properties", out var dynamicProperties))
             {
                 throw new ArgumentException("The given JSON document does not contain a properties object");
             }
-            
+
+            // The vid is the "id" of the entity
+            if (expandoDict.TryGetValue("vid", out var vidData))
+            {
+                var vidProp = dtoProps.SingleOrDefault(q => q.Name.ToLowerInvariant() == "vid");
+                vidProp?.SetValue(dto, vidData);
+            }
+
             foreach (var dynamicProp in dynamicProperties as ExpandoObject)
             {
                 // prop.Key contains the name of the property we wish to map into the DTO
                 // prop.Value contains the data returned by HubSpot, which is also an object 
                 // in there we need to go get the "value" prop to get the actual value
                 _logger.LogDebug("Looking at dynamic prop: {0}", dynamicProp.Key);
-                
-                if (!((IDictionary<string, Object>) dynamicProp.Value).TryGetValue("value", out object dynamicValue))
+
+                if (!((IDictionary<string, Object>)dynamicProp.Value).TryGetValue("value", out object dynamicValue))
                 {
                     continue;
                 }
-                
+
                 var targetProp =
-                    dataProps.SingleOrDefault(q => q.Name.ToLowerInvariant() == dynamicProp.Key.ToLowerInvariant());
-                _logger.LogDebug("Have target prop? '{0}' with name: '{1}' and actual value: '{2}'", targetProp != null, targetProp?.Name ?? "N/A", dynamicValue);
-                targetProp?.SetValue(data, dynamicValue);
+                    dtoProps.SingleOrDefault(q => q.Name.ToLowerInvariant() == dynamicProp.Key.ToLowerInvariant());
+                _logger.LogDebug("Have target prop? '{0}' with name: '{1}' and actual value: '{2}'", targetProp != null,
+                    targetProp?.Name ?? "N/A", dynamicValue);
+                targetProp?.SetValue(dto, dynamicValue);
             }
-            return data;
+            return dto;
         }
+
     }
 }
--- test/unit/Requests/RequestDataConverterTest.cs
+++ test/unit/Requests/RequestDataConverterTest.cs
@@ -1,5 +1,7 @@
 ﻿using System.Dynamic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Skarp.HubSpotClient.Dto;
@@ -122,9 +124,35 @@ namespace Skarp.HubSpotClient.UnitTest
             Assert.Equal(contact.ZipCode, "02139");
         }
 
+        [Fact]
+        public void RequestDataConvert_converts_list_response_to_entities()
+        {
+            var json =
+                "{\"contacts\":[{\"addedAt\":1390574181854,\"vid\":204727,\"canonical-vid\":204727,\"merged-vids\":[],\"portal-id\":62515,\"is-contact\":true,\"profile-token\":\"AO_T-mMusl38dq-ff-Lms9BvB5nWgFb7sFrDU98e-3CBdnB7G2qCt1pMEHC9zmqSfOkeq2on6Dz72P-iLoGjEXfLuWfvZRWBpkB-C9Enw6SZ-ZASg57snQun5f32ISDfLOiK7BYDL0l2\",\"profile-url\":\"https://app.hubspot.com/contacts/62515/lists/public/contact/_AO_T-mMusl38dq-ff-Lms9BvB5nWgFb7sFrDU98e-3CBdnB7G2qCt1pMEHC9zmqSfOkeq2on6Dz72P-iLoGjEXfLuWfvZRWBpkB-C9Enw6SZ-ZASg57snQun5f32ISDfLOiK7BYDL0l2/\",\"properties\":{\"firstname\":{\"value\":\"Bob\"},\"lastmodifieddate\":{\"value\":\"1483461406481\"},\"company\":{\"value\":\"\"},\"lastname\":{\"value\":\"Record\"}},\"form-submissions\":[],\"identity-profiles\":[{\"vid\":204727,\"saved-at-timestamp\":1476768116149,\"deleted-changed-timestamp\":0,\"identities\":[{\"type\":\"LEAD_GUID\",\"value\":\"f9d728f1-dff1-49b0-9caa-247dbdf5b8b7\",\"timestamp\":1390574181878},{\"type\":\"EMAIL\",\"value\":\"mgnew-email@hubspot.com\",\"timestamp\":1476768116137}]}],\"merge-audits\":[]},{\"addedAt\":1392643921079,\"vid\":207303,\"canonical-vid\":207303,\"merged-vids\":[],\"portal-id\":62515,\"is-contact\":true,\"profile-token\":\"AO_T-mPMwvuZG_QTNH28c_MbhSyNRuuTNw9I7zJAaMFjOqL9HKlH9uBteqHAiTRUWVAPTThuU-Fmy7IemUNUvdtYpLrsll6nw47qnu7ACiSHFR6qZP1tDVZFpxueESKiKUIIvRjGzt8P\",\"profile-url\":\"https://app.hubspot.com/contacts/62515/lists/public/contact/_AO_T-mPMwvuZG_QTNH28c_MbhSyNRuuTNw9I7zJAaMFjOqL9HKlH9uBteqHAiTRUWVAPTThuU-Fmy7IemUNUvdtYpLrsll6nw47qnu7ACiSHFR6qZP1tDVZFpxueESKiKUIIvRjGzt8P/\",\"properties\":{\"firstname\":{\"value\":\"Ff_FirstName_0\"},\"lastmodifieddate\":{\"value\":\"1479148429488\"},\"lastname\":{\"value\":\"Ff_LastName_0\"}},\"form-submissions\":[],\"identity-profiles\":[{\"vid\":207303,\"saved-at-timestamp\":1392643921090,\"deleted-changed-timestamp\":0,\"identities\":[{\"type\":\"EMAIL\",\"value\":\"email_0be34aebe5@abctest.com\",\"timestamp\":1392643921079},{\"type\":\"LEAD_GUID\",\"value\":\"058378c6-9513-43e1-a13a-43a98d47aa22\",\"timestamp\":1392643921082}]}],\"merge-audits\":[]}],\"has-more\":true,\"vid-offset\":207303}";
+
+            var jobj = JsonConvert.DeserializeObject<ExpandoObject>(json);
+
+            var contactList = _converter.FromHubSpotListResponse<ContactListHubSpotEntity<ContactHubSpotEntity>>(jobj);
+
+            Assert.NotNull(contactList);
+            Assert.True(contactList.MoreResultsAvailable);
+            Assert.InRange(contactList.ContinuationOffset, 1, long.MaxValue);
+            Assert.True(2 == contactList.Contacts.Count, $"2 == contactList.Contacts.Count - have count {contactList.Contacts.Count}");
+
+            var bob = contactList.Contacts.First();
+            Assert.Equal("Bob", bob.FirstName);
+            Assert.Equal("Record", bob.Lastname);
+
+            var other = contactList.Contacts.Last();
+            Assert.Equal("Ff_FirstName_0", other.FirstName);
+            Assert.Equal("Ff_LastName_0", other.Lastname);
+        }
+
         private class CustomContactHubSpotEntity : ContactHubSpotEntity
         {
             public string MyCustomProp { get; set; }
         }
+
+       
     }
 }
