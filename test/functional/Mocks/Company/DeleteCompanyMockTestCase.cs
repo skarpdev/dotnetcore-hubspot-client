381817b909e3da841bb41edc2faea3586d9eda7d
Joachim Schiødtz <jsc@mavins.io>
2017-09-15 15:46:32 +0200

    Added CRUD implementation for Company interface

---
+++ test/functional/Mocks/Company/DeleteCompanyMockTestCase.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using RapidCore.Network;
+using Skarp.HubSpotClient.Core;
+
+namespace Skarp.HubSpotClient.FunctionalTests.Mocks.Company
+{
+    public class DeleteCompanyMockTestCase : IMockRapidHttpClientTestCase
+    {
+        public bool IsMatch(HttpRequestMessage request)
+        {
+            return request.RequestUri.AbsolutePath.Contains("/companies/v2/companies/10444744") && request.Method == HttpMethod.Delete;
+        }
+
+        public Task<HttpResponseMessage> GetResponseAsync(HttpRequestMessage request)
+        {
+            const string jsonResponse = "";
+
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new JsonContent(jsonResponse),
+                StatusCode = HttpStatusCode.NoContent,
+                RequestMessage = request
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
---
+++ test/unit/Company/HubSpotCompanyClientTest.cs
@@ -0,0 +1,80 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FakeItEasy;
+using RapidCore.Network;
+using Skarp.HubSpotClient.Core;
+using Skarp.HubSpotClient.Core.Interfaces;
+using Skarp.HubSpotClient.Core.Requests;
+using Xunit;
+using Xunit.Abstractions;
+using Skarp.HubSpotClient.Company;
+using Skarp.HubSpotClient.Company.Dto;
+
+namespace Skarp.HubSpotClient.UnitTest.Company
+{
+    public class HubSpotCompanyClientTest : UnitTestBase<HubSpotCompanyClient>
+    {
+        private readonly HubSpotCompanyClient _client;
+        private IRapidHttpClient _mockHttpClient;
+        private RequestSerializer _mockSerializer;
+
+        public HubSpotCompanyClientTest(ITestOutputHelper output) : base(output)
+        {
+            _mockHttpClient = A.Fake<IRapidHttpClient>(opts => opts.Strict());
+
+            A.CallTo(() => _mockHttpClient.SendAsync(A<HttpRequestMessage>.Ignored))
+                .Returns(Task.FromResult(CreateNewEmptyOkResponse()));
+
+            _mockSerializer = A.Fake<RequestSerializer>(opts => opts.Strict());
+            A.CallTo(() => _mockSerializer.SerializeEntity(A<CompanyHubSpotEntity>.Ignored))
+                .Returns("{}");
+
+            A.CallTo(() => _mockSerializer.DeserializeEntity<CompanyHubSpotEntity>(A<string>.Ignored))
+                .Returns(new CompanyHubSpotEntity());
+
+            _client = new HubSpotCompanyClient(
+                _mockHttpClient,
+                Logger,
+                _mockSerializer,
+                "https://api.hubapi.com",
+                "HapiKeyFisk"
+                );
+        }
+
+        private HttpResponseMessage CreateNewEmptyOkResponse()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new JsonContent("{}")
+            };
+            return response;
+        }
+
+        [Theory]
+        [InlineData(HubSpotAction.Create, "/companies/v2/companies")]
+        [InlineData(HubSpotAction.Get, "/companies/v2/companies/:companyId:")]
+        [InlineData(HubSpotAction.Update, "/companies/v2/companies/:companyId:")]
+        [InlineData(HubSpotAction.Delete, "/companies/v2/companies/:companyId:")]
+        public void CompanyClient_path_resolver_works(HubSpotAction action, string expetedPath)
+        {
+            var resvoledPath = _client.PathResolver(new CompanyHubSpotEntity(), action);
+            Assert.Equal(expetedPath, resvoledPath);
+        }
+
+        [Fact]
+        public async Task CompanyClient_create_contact_work()
+        {
+            var response = await _client.CreateAsync<CompanyHubSpotEntity>(new CompanyHubSpotEntity
+            {
+                Name = "A new Company",
+                Description = "A new description"                
+            });
+
+            A.CallTo(() => _mockHttpClient.SendAsync(A<HttpRequestMessage>.Ignored)).MustHaveHappened();
+            A.CallTo(() => _mockSerializer.SerializeEntity(A<IHubSpotEntity>.Ignored)).MustHaveHappened();
+            A.CallTo(() => _mockSerializer.DeserializeEntity<CompanyHubSpotEntity>("{}")).MustHaveHappened();
+        }
+
+    }
+}
