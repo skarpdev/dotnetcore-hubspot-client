df10f25000b523964aebe6ea648d653018c80dbf
Nicklas Laine Overgaard <nicklas@skarp.dk>
2017-10-12 20:39:13 +0200

    Add integration test suite with test of GetContact
    In order to test whether the issue reported in #1 can be replicated

---
+++ test/integration/Contact/HubSpotContactClientIntegrationTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using RapidCore.Network;
+using Skarp.HubSpotClient.Contact;
+using Skarp.HubSpotClient.Contact.Dto;
+using Skarp.HubSpotClient.Core.Requests;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace integration.Contact
+{
+    public class HubSpotContactClientIntegrationTest : IntegrationTestBase<HubSpotContactClient>
+    {
+        private readonly HubSpotContactClient _client;
+
+        public HubSpotContactClientIntegrationTest(ITestOutputHelper output) : base(output)
+        {
+            _client = new HubSpotContactClient(
+                new RealRapidHttpClient(new HttpClient()),
+                base.Logger,
+                new RequestSerializer(new RequestDataConverter(LoggerFactory.CreateLogger<RequestDataConverter>())),
+                "https://api.hubapi.com",
+                "demo");
+        }
+
+        [Fact]
+        public async Task Get_non_existing_contact_works()
+        {
+            var contact = await _client.GetByEmailAsync<ContactHubSpotEntity>("iamnothere@skarp.dk");
+            Assert.Null(contact);
+        }
+    }
+}
---
+++ test/integration/IntegrationTestBase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using RapidCore.Xunit.Logging;
+using Xunit.Abstractions;
+
+namespace integration
+{
+    public abstract class IntegrationTestBase<T>
+    {
+        protected readonly ITestOutputHelper Output;
+        protected readonly LoggerFactory LoggerFactory;
+        protected readonly ILogger<T> Logger;
+
+        protected IntegrationTestBase(ITestOutputHelper output)
+        {
+            Output = output;
+            LoggerFactory = new LoggerFactory();
+            LoggerFactory.AddXunitOutput(output);
+
+            Logger = LoggerFactory.CreateLogger<T>();
+        }
+    }
+}
