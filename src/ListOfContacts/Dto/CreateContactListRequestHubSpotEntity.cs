4cfe715dbedc6810fc656c806a0c4e0657939b24
davymirfin <davymirfin@gmail.com>
2021-01-21 13:08:18 +0100

    Remove commented out code.

--- src/ListOfContacts/Dto/CreateContactListRequestHubSpotEntity.cs
+++ src/ListOfContacts/Dto/CreateContactListRequestHubSpotEntity.cs
@@ -9,27 +9,19 @@ using System.Text;
 namespace Skarp.HubSpotClient.ListOfContacts.Dto
 {
     public class CreateContactListRequestHubSpotEntity : ICreateContactListRequestHubSpotEntity
-    //IHubSpotEntity
-    //, ICreateContactListRequestHubSpotEntity
     {
         [DataMember(Name = "name")]
-        //[JsonProperty("name")]
         public string Name { get; set; }
         [DataMember(Name = "dynamic")]
-        //[JsonProperty("dynamic")]
         public bool? Dynamic { get; set; }
         [DataMember(Name = "portalId")]
-        //[JsonProperty("portalId")]
         public int? PortalId { get; set; }
         [DataMember(Name = "filters")]
-        //[JsonProperty("filters")]
         public List<List<IContactListFilter>> Filters { get; set; }
 
-        //[JsonProperty("offset")]
         public int offset { get; set; }
 
         [DataMember(Name = "has-more")]
-        //[JsonProperty("has-more")]
         public bool HasMore { get; set; }
 
         public bool IsNameValue => false;
