b4f46828fdd91f5bd934cd0a6f57ac9bbd825cc3
Lee Stevens <leestevens318@gmail.com>
2021-05-20 18:09:21 +1000

    Issues #78

---
+++ test/integration/Contact/Dto/ContactHubSpotEntityExtended.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.Serialization;
+using Skarp.HubSpotClient.Contact.Dto;
+
+namespace integration.Contact.Dto
+{
+    /// <summary>
+    /// Extended HubSpot Contact Entity
+    /// </summary>
+    /// <remarks>Used to test functionality of DateTime/DateTime?/DateTimeOffset/DateTimeOffset?</remarks>
+    [DataContract]
+    public class ContactHubSpotEntityExtended : ContactHubSpotEntity
+    {
+        [DataMember(Name = "lastmodifieddate")]
+        public DateTimeOffset? LastModified { get; set; }
+    }
+}
